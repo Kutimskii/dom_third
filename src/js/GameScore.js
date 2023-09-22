04dead53012d7f7582ecc491e27a0c18aefad776
kkk <kutimskyi@gmail.com>
2023-09-22 15:50:29 +0300

    second

--- src/js/GameScore.js
+++ src/js/GameScore.js
@@ -1,5 +1,7 @@
+import GamePlay from "./GamePlay";
 export default class GameScore {
   constructor() {
+    this.GamePlay = new GamePlay();
     this.score = 0;
     this.missed = 0;
     this.HtmlScore = document.querySelector(".scores-number");
@@ -8,13 +10,20 @@ export default class GameScore {
   }
   countScore() {
     this.board.addEventListener("click", (event) => {
-      console.log();
       if (event.target.className === "img") {
         this.score += 1;
         this.HtmlScore.textContent = this.score;
       } else {
         this.missed += 1;
         this.HtmlMissedScore.textContent = this.missed;
+      }
+      if (this.missed === 5) {
+        clearInterval();
+        this.score = 0;
+        this.missed = 0;
+        this.HtmlMissedScore.textContent = this.missed;
+        this.HtmlScore.textContent = this.score;
+        alert("Конец игры");
       }
     });
   }
